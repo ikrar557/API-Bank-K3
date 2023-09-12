837a08e15fd7d3f779962ad26f2e91324a997bfa
alpinmohsa541 <alpinmohsa@gmail.com>
2023-09-12 22:21:15 +0700

    membuat logika menampilkan data dan convert ke json

--- getApi.php
+++ getApi.php
@@ -5,4 +5,19 @@ include "conn.php";
 $sql = "SELECT * FROM TblAkun";
 $query = mysqli_query($conn, $sql);
 
+while($data = mysqli_fetch_array($query)){
+
+    $item[] = array(
+        'idAkun' => $data ["idAkun"],
+        'Nama' => $data ["nama"],
+        'norek' => $data ["norek"],
+        'saldo' => $data ["saldo"],
+    );
+}
+    $response = array(
+        'status' => 'OK',
+        'data' =>$item
+    );
+
+    echo json_encode($response);
 ?>
