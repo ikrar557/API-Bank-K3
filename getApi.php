f7e47ef09f45694029cfcb0091b67e0b345bb28d
alpinmohsa541 <alpinmohsa@gmail.com>
2023-09-13 11:31:04 +0700

    create mengambil data dengean kriteria tertentu

--- getApi.php
+++ getApi.php
@@ -2,22 +2,57 @@
 //koneksi ke database
 include "conn.php";
 
-$sql = "SELECT * FROM TblAkun";
-$query = mysqli_query($conn, $sql);
+if(isset($_GET['idAkun'])){
+    $idAkun = $_GET['idAkun'];
 
-while($data = mysqli_fetch_array($query)){
-
-    $item[] = array(
-        'idAkun' => $data ["idAkun"],
-        'Nama' => $data ["nama"],
-        'norek' => $data ["norek"],
-        'saldo' => $data ["saldo"],
-    );
+    $sql = "SELECT * FROM TblAkun WHERE idAkun = $idAkun";
+} else{
+    $sql = "SELECT * FROM TblAkun";
 }
-    $response = array(
+
+$result = $conn-> query($sql);
+
+// $sql = "SELECT * FROM TblAkun";
+// $query = mysqli_query($conn, $sql);
+
+// if($hasil->$num_row>0){
+//     $data = array();
+//     while($data = $hasil -> fetch_assoc()){
+
+//         $item[] = array(
+//             'idAkun' => $data ["idAkun"],
+//             'Nama' => $data ["nama"],
+//             'norek' => $data ["norek"],
+//             'saldo' => $data ["saldo"],
+//         );
+//     }
+//         $response = array(
+//             'status' => 'OK',
+//             'data' =>$item
+//         );
+    
+// }
+if ($result) {
+    // Memeriksa apakah query berhasil dijalankan
+    if ($result->num_rows > 0) {
+        // Mengambil data dari hasil query
+        while ($row = $result->fetch_assoc()) {
+            $item[] = array(
+                'idAkun' => $row ["idAkun"],
+                'Nama' => $row ["nama"],
+                'norek' => $row ["norek"],
+                'saldo' => $row ["saldo"],
+            );
+        }
+    } $response = array(
         'status' => 'OK',
         'data' =>$item
     );
-
+    
+    // Menutup koneksi
+    $conn->close();
+} else {
+    echo "Terjadi kesalahan dalam query: " . $conn->error;
+}
     echo json_encode($response);
 ?>
